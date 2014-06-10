8f2e5b5980e4e59f0f09de55f2b032d3e2ee2666
mgoann@126.com <mgoann@126.com>
2014-06-10 22:05:26 +0800

    commit deployfile.bat

--- src/build/poms/deployfile.bat
+++ src/build/poms/deployfile.bat
@@ -1,4 +1,4 @@
-﻿mvn install:install-file -Dfile=jpf-1.5.1.jar -DgroupId=net.sf.jpf -DartifactId=jpf -Dversion=1.5.1 -Dpackaging=jar 
+mvn install:install-file -Dfile=jpf-1.5.1.jar -DgroupId=net.sf.jpf -DartifactId=jpf -Dversion=1.5.1 -Dpackaging=jar 
 mvn install:install-file -Dfile=bcmail-jdk16-146.jar -DgroupId=bouncycastle -DartifactId=bcmail-jdk16 -Dversion=146 -Dpackaging=jar 
 mvn install:install-file -Dfile=bcprov-jdk16-146.jar -DgroupId=bouncycastle -DartifactId=bcprov-jdk16 -Dversion=146 -Dpackaging=jar 
 mvn install:install-file -Dfile=bctsp-jdk16-146.jar -DgroupId=bouncycastle -DartifactId=bctsp-jdk16 -Dversion=146 -Dpackaging=jar  
@@ -8,31 +8,16 @@ mvn install:install-file -Dfile=betelio-1.0.jar -DgroupId=betelio -DartifactId=b
 mvn install:install-file -Dfile=mail-1.5.0.jar -DgroupId=javax.mail -DartifactId=mail -Dversion=1.5.0 -Dpackaging=jar
 mvn install:install-file -Dfile=jcr-1.0.1.jar -DgroupId=javax.jcr -DartifactId=jcr -Dversion=1.0.1 -Dpackaging=jar
 mvn install:install-file -Dfile=stax2-api-3.1.1.jar -DgroupId=org.jvnet -DartifactId=staxex -Dversion=stax-ex -Dpackaging=jar
-mvn install:install-file -Dfile=stax2-api-3.1.1.jar -DgroupId=com.google.gwt -DartifactId=gwt-incubator -Dversion=2.1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=gwt-incubator-20101117-r1766.jar -DgroupId=com.google.gwt -DartifactId=gwt-incubator -Dversion=2.1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=gwt-servlet-2.5.1.jar -DgroupId=com.google.gwt -DartifactId=gwt-servlet -Dversion=2.1.0 -Dpackaging=jar
-
-
-com.google.gwt:gwt-servlet:jar:2.5.1 (compile)
-com.google.gwt:gwt-user:jar:2.5.1 (provided)
-com.googlecode.gwtupload:gwtupload:jar:0.6.6 (compile)
-com.google.gwt:gwt-incubator:jar:2.1.0 (compile)
-com.allen_sauer.gwt.dnd:gwt-dnd:jar:3.0.1 (compile)
-org.timepedia.exporter:gwtexporter:jar:2.0.10 (compile)
-gwtchismes:gwtchismes:jar:0.9.8 (compile)
-com.smartgwt:smartgwt:jar:4.0 (compile)
-com.smartgwt:smartgwt-skins:jar:4.0 (provided)
-com.bramosystems.oss.player:bst-player-api:jar:1.2 (provided)
-com.orange.links:gwt-links:jar:1.4.2(compile)
-com.reveregroup.gwt:gwt-image-loader:jar:1.1.3 (provided)
-ofcgwt:ofcgwt:jar:1.3.0 (compile)
-org.slf4j:slf4j-log4j12:jar:1.6.4 (compile)
-commons-codec:commons-codec:jar:1.6 (compile)
-commons-fileupload:commons-fileupload:jar:1.2.1 (compile)
-commons-digester:commons-digester:jar:1.8 (compile)
-com.logicaldoc:logicaldoc-i18n:jar:6.8.4 (compile)
-javax.servlet:servlet-api:jar:2.5 (provided)
-commons-validator:commons-validator:jar:1.3.1 (provided)
-commons-lang:commons-lang:jar:2.4 (compile)
-commons-collections:commons-collections:jar:3.2 (compile)
-commons-beanutils:commons-beanutils:jar:1.7.0 (compile)
-commons-io:commons-io:jar:1.4 (compile)]:
+mvn install:install-file -Dfile=gwtupload-0.6.6.jar -DgroupId=com.googlecode.gwtupload -DartifactId=gwtupload -Dversion=0.6.6 -Dpackaging=jar
+mvn install:install-file -Dfile=gwt-dnd-3.1.0.jar -DgroupId=com.allen_sauer.gwt.dnd -DartifactId=gwt-dnd -Dversion=3.0.1 -Dpackaging=jar
+mvn install:install-file -Dfile=gwtexporter-2.1.0.jar -DgroupId=org.timepedia.exporter -DartifactId=gwtexporter -Dversion=2.0.10 -Dpackaging=jar
+mvn install:install-file -Dfile=gwtchismes-0.9.7.jar -DgroupId=gwtchismes -DartifactId=gwtchismes -Dversion=0.9.8 -Dpackaging=jar
+mvn install:install-file -Dfile=smartgwt.jar -DgroupId=com.smartgwt -DartifactId=smartgwt -Dversion=4.0 -Dpackaging=jar
+mvn install:install-file -Dfile=smartgwt-skins.jar -DgroupId=com.smartgwt -DartifactId=smartgwt-skins -Dversion=4.0 -Dpackaging=jar
+mvn install:install-file -Dfile=bst-player-api-1.2.jar -DgroupId=com.bramosystems.oss.player -DartifactId=bst-player-api -Dversion=1.2 -Dpackaging=jar
+mvn install:install-file -Dfile=gwt-links-1.4.2.jar -DgroupId=com.orange.links -DartifactId=gwt-links -Dversion=1.4.2 -Dpackaging=jar
+mvn install:install-file -Dfile=gwt-image-loader-1.1.4.jar -DgroupId=com.reveregroup.gwt -DartifactId=gwt-image-loader -Dversion=1.1.3 -Dpackaging=jar
+mvn install:install-file -Dfile=ofcgwt.jar -DgroupId=ofcgwt -DartifactId=ofcgwt -Dversion=1.3.0 -Dpackaging=jar
+mvn install:install-file -Dfile=ofcgwt.jar -DgroupId=ofcgwt -DartifactId=ofcgwt -Dversion=1.3.0 -Dpackaging=jar
